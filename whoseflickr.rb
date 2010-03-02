64b64f6ef0952920950300472e086bda75eaf5a5
Alex Kahn <alexanderkahn@gmail.com>
2010-03-01 22:12:25 -0500

    Show profile.

--- whoseflickr.rb
+++ whoseflickr.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 require 'net/http'
-require 'json'
+require 'crack/xml'
 
 INFO_URI = "http://api.flickr.com/services/rest/"
 QUERY = "method=flickr.photos.getInfo&api_key=197e2adb7e0ffd30a7441537726f756f"
@@ -10,7 +10,8 @@ PATTERN = %r|http://farm\d+.static.flickr.com/\d+/(\d+)_.+.jpg|
 
 get '/' do
   if params[:url]
-    @profile = photo_profile(params[:url])
+    @profile = fetch_profile(params[:url])
+    return @profile if request.xhr?
   end
   haml :index
 end
@@ -21,10 +22,15 @@ get '/screen.css' do
 end
 
 helpers do
-  def photo_profile(url)
+  def fetch_profile(url)
     return unless id = url.match(PATTERN)
     uri = URI.parse(INFO_URI)
     uri.query = QUERY + "&photo_id=" + id.captures.first
-    Net::HTTP.get(uri)
+    profile_markup(Crack::XML.parse(Net::HTTP.get(uri)))
+  end
+
+  def profile_markup(hash)
+    @profile = hash['rsp']
+    haml :profile
   end
 end
